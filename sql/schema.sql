b35e12839448495a64886072b2e8d6ec791e086c
Moupriya Samanta <108943431+moupriyasamanta21@users.noreply.github.com>
2025-12-19 16:49:03 +0530

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -14,4 +14,12 @@ CREATE TABLE bookings (
 );
 
 -- Insert 10 sample seats
+
 INSERT INTO seats (seat_id) SELECT generate_series(1, 10);
+
+booking_db=# update seats
+booking_db-# set status = 'BOOKED', version = version + 1
+booking_db-# where seat_id = 1 and version = 0;
+
+SELECT * FROM seats;
+SELECT * FROM bookings;
